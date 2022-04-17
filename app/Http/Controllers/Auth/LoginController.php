d3a18b0b5872598124c1388cd9eea75c27a53bca
Abdul Hafidh <abdul_h20@mhs.unsyiah.ac.id>
2022-04-17 17:45:27 +0700

    Testing
    Generate Set Up and Tear Down

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -7,14 +7,21 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
+    /**
+     * Sebuah Method Controller yang digunakan untuk menampilkan halaman login
+     * @return view halaman login yang berisi form login karena routing yang diterima adalah /login
+     */
     public function index()
     {
         return view('auth.login',[
             "title" => "Login"
         ]);
     }
-
+    /**
+     * Sebuah Method Controller yang digunakan untuk melakukan proses login
+     * 
+     */
     public function login(){
-        dd('Login');
+        dd('SELAMAT DATANG KE DALAM APLIKASI HOSPIRESC ^_^');
     }
 }
