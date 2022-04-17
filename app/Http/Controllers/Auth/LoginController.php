ffebe266876cdcb34291c79265e92088fbc3ae8d
Abdul Hafidh <abdul_h20@mhs.unsyiah.ac.id>
2022-04-17 17:26:12 +0700

    Switch Route Content to Controller

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -7,5 +7,14 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('auth.login',[
+            "title" => "Login"
+        ]);
+    }
+
+    public function login(){
+        dd('Login');
+    }
 }
