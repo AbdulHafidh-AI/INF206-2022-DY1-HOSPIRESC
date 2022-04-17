a98a4b24269560505bb585d35b5916541a9ed9e6
rahminajla <rahmi_nj@mhs.unsyiah.ac.id>
2022-04-18 04:21:58 +0700

    add image file, add frame photo and line in page about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -11,32 +11,38 @@
   </div>
 {{-- End of Page Title --}}
   
+{{-- Up Line --}}
+<hr> <hr>
+{{-- End of up line --}}
 
 {{-- Content --}}
 
   <div class="grid">
     <div class="row"> 
-      <div class="col-md-3">
-        <div class="box"></div> 
-          <img src="/assets/images/icut.jpeg" alt="" style="width:100%">
+      <div class="col-md-2">
+        <div class="img"></div> 
+          <img src="/assets/images/icut.jpeg" alt="" style="width:100%">   
+          <img src="/assets/images/frame-foto.png" alt="" style="width:100%">
         </div>
           <p class="title"></p>
           <p>Nama: Cut Nadilla Maretna</p>
           <p>Umur: 19 Tahun</p>
           <p>Alamat: Jln. Prada</p>
       
-        <div class="col-md-3">
+        <div class="col-md-2">
           <div class="box"></div> 
           <img src="/assets/images/lala.jpg" alt="" style="width:100%">
+          <img src="/assets/images/frame-foto.png" alt="" style="width:100%">
         </div>
           <p class="title"></p>
           <p>Nama: Rahmi Najla</p>
           <p>Umur: 19 Tahun</p>
           <p>Alamat: Jln. Elang</p>
       
-        <div class="col-md-3">
+        <div class="col-md-2">
           <div class="box"></div> 
           <img src="/assets/images/upak.jpeg" alt="" style="width:100%">
+          <img src="/assets/images/frame-foto.png" alt="" style="width:100%">
         </div>
             <p class="title"></p>
             <p>Nama: Ulfa Rahmah</p>
@@ -45,28 +51,33 @@
     </div>
  
     <div class="row">
-        <div class="col-md-3">
+        <div class="col-md-2">
           <div class="box"></div> 
           <img src="/assets/images/abdul.jpeg" alt="" style="width:100%">
+          <img src="/assets/images/frame-foto.png" alt="" style="width:100%">
         </div>
             <p class="title"></p>
-            <h3>Nama: Abdul Hafidh</h3>
-            <h3>Umur: 19 Tahun</h3>
-            <h3>Alamat: Jln. </h3>
+            <p>Nama: Abdul Hafidh</p>
+            <p>Umur: 19 Tahun</p>
+            <p>Alamat: Jln. </p>
 
-        <div class="col-md-3">
+        <div class="col-md-2">
           <div class="box"></div> 
           <img src="/assets/images/faiz.jpeg" alt="" style="width:100%">
+          <img src="/assets/images/frame-foto.png" alt="" style="width:100%">
         </div>
             <p class="title"></p>
-            <h3>Nama: Faiz Dzakirullail</h3>
-            <h3>Umur: 19 Tahun</h3>
-            <h3>Alamat: Jln. </h3>            
+            <p>Nama: Faiz Dzakirullail</p>
+            <p>Umur: 19 Tahun</p>
+            <p>Alamat: Jln. </p>            
     </div>
   </div>
   {{-- End of content --}}
     
-          
+{{-- Bottom Line --}}
+<hr> <hr>
+{{-- End of bottom line --}}
+      
       
 
     
