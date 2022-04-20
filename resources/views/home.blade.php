78f818eb593cd6ebdde52fdb0c439df7b18624be
Cut Nadilla Maretna <cut_nm@mhs.unsyiah.ac.id>
2022-04-20 12:46:08 +0700

    add code

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,3 +2,40 @@
 
 @include('partials.navbar')
 
+<link rel="stylesheet" href="style.css">
+
+<body style="background-color:#2F303A;"></body>
+
+<body>
+    <div class="wrapper"> 
+        <div class="container">
+            <div class="kolom">
+                <div class="text-center"></div>
+                <p class="deskripsi">
+                    <h1>Minta Tolong Jadi<br>
+                    Lebih Mudah dengan <br>
+                    HOSPIRESC</h1>
+                </p>
+
+                <div class="text-center"></div>
+                <p class="des1" style="color:whitesmoke;">
+                    <h3>HOSPIRESC adalah layanan aplikasi berbasis web yang<br>
+                    diperuntukkan bagi  setiap rumah sakit di seluruh<br> 
+                    Indonesia.
+                    </h3>
+                </p>
+                
+
+                <p class="des2style=" style="color:whitesmoke;">
+                    <h3> HOSPIRESC sebagai penghubung antar rumah sakit di<br>    
+                    dalam  suatu forum aplikasi agar dapat saling<br> 
+                    melakukan interaksi  mengenai kebutuhan peralatan<br> 
+                    sebagai penunjang peralatan  dan pelayanan pasien.</h3>
+
+                </p>
+
+                <img src="/assets/images/doctor-dashboard.jpg" alt="" width="80" height="100">
+            </div>
+         </div>
+    </div>
+</body>
