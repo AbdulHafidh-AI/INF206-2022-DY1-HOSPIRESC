a1d4563dbbd7fbb9f5b55afb42c3caa0fbebe590
Abdul Hafidh <abdul_h20@mhs.unsyiah.ac.id>
2022-04-20 15:02:10 +0700

    Restying a page

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -4,16 +4,19 @@
 
 @section('content')
     <!-- Add your site or application content here -->
+    <div class="container mt-5 gambarAwal">
+        <div class="gambar">
+            <img src="assets/images/kumpulan-doctor-removebg-preview.png" class="rounded float-start" alt="tes">
+        </div>
 
-    <div class="container mt-5">
-        <img src="assets/images/kumpulan-doctor-removebg-preview.png" class="rounded float-start" alt="tes">
         <!-- Add explanation right side of the image -->
-        <div class="container">
+        <div class="container fitur ms-auto">
             <div class="text-center">
                 <h1>SELAMAT DATANG!</h1>
                 <div class="text-center">
                     <h4>Lakukan Pengajuan Kebutuhan <br> Yang Anda Perlukan!</h4>
                 </div>
+                <br>
                 <div class="text-center">
                     <p class="groove">
                         Working Hours
@@ -21,9 +24,15 @@
                         Senin-Jum’at: 09.00-17.00
                         Sabtu-Minggu: 10.30-15.00
                     </p>
-                     <!-- Button for Log In -->
-                     <div class="text-center">
-                        <button type="button">Ajukan</button>
+                    <br>
+                    <!-- Button for Log In -->
+                    <div class="text-center">
+                        <a href="/mengajukan"><button type="button">
+                                <div class="text-ajukan">
+                                    Ajukan
+                                </div>
+                            </button>
+                        </a>
                         </h6>
                         <!-- End Of Button for Log In -->
                     </div>
