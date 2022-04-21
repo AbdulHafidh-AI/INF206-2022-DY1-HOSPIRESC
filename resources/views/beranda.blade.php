41ab27374bb4a26eb1b301f8d9f12c28af5a5be8
Abdul Hafidh <abdul_h20@mhs.unsyiah.ac.id>
2022-04-21 16:26:28 +0700

    Restyling a page

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -4,13 +4,13 @@
 
 @section('content')
     <!-- Add your site or application content here -->
-    <div class="container mt-5 gambarAwal">
+    <div class="container mt-2 gambarAwal">
         <div class="gambar">
-            <img src="assets/images/kumpulan-doctor-removebg-preview.png" class="rounded float-start" alt="tes">
+            <img src="assets/images/kumpulan-doctor-removebg-preview.png" width="1000" class="rounded float-start" alt="tes">
         </div>
 
         <!-- Add explanation right side of the image -->
-        <div class="container fitur ms-auto">
+        <div class="container mt-4 ms-auto">
             <div class="text-center">
                 <h1>SELAMAT DATANG!</h1>
                 <div class="text-center">
@@ -21,7 +21,7 @@
                     <p class="groove">
                         Working Hours
                         <br>
-                        Senin-Jum’at: 09.00-17.00
+                        Senin-Jum’at: 09.00-17.00<br>
                         Sabtu-Minggu: 10.30-15.00
                     </p>
                     <br>
