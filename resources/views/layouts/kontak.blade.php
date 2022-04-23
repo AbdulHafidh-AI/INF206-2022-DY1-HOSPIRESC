a07409ed4a2d3deb2c51f94f82b94adb60296bed
Abdul Hafidh <abdul_h20@mhs.unsyiah.ac.id>
2022-04-23 13:48:50 +0700

    KONTAK DI REVISI

--- resources/views/layouts/kontak.blade.php
+++ resources/views/layouts/kontak.blade.php
@@ -14,6 +14,7 @@
 
     <link rel="stylesheet" href="/assets/custom/template/style.css">
     <link rel="stylesheet" href="/assets/custom/login/style.css">
+    <link rel="stylesheet" href="/assets/custom/kontak/style.css">
 
     <!-- favicon -->
     <link rel="shortcut icon" href="/assets/icon//favicon.ico" type="image/x-icon">
--- resources/views/pages/kontak.blade.php
+++ resources/views/pages/kontak.blade.php
@@ -1,32 +1,27 @@
-@extends('layouts.home')
+@extends('layouts.kontak')
 
 @include('partials.navbar2')
 
 
 @section('content')
-
-
-
-      <h1>Kontak</h1>
-      <ul>
-        <p>&nbsp;&nbsp;Hubungi Kami</p>
-        <div class="p-2">
-        <p>Jl. Jend. Ahmad Yani No.40
-            Peunayong, Kec. Kuta Alam, Kota Banda Aceh, Aceh 
-            </p>
-        <div class="p-2  text-white">
-        <img src="icon/Massage.jpeg" height="35px" width="30px" href="assets/icon/Massage.jpeg" class="fs-4 text-light" >&nbsp;Hospirec2022@gmail.com</img></div>
-        <div class="p-2 text-white">
-        <img src="icon/Kontak.jpeg" height="35px" width="30px" href="assets/icon/Kontak.jpeg" class="fs-4" >&nbsp;0651-9795</img></div>
-        <div class="p-2 text-white">
-        <img src="icon/Twitter.jpeg" height="35px" width="30px" href="assets/icon/Twitter.jpeg" class="fs-4">&nbsp;@Hospirec2022</img></div>
-        <div></div>
-      </ul>
+<div class="container">
+    <div class="container mt-2">
+        <div class=" text-start mt-5">
+            <h1 class="text-bold text-white">KONTAK</h1>
+            <h2 class="text-bold">HUBUNGI KAMI MELALUI:</h2>
+            <a>
+                <p class="text-white">  <img src="assets/icon/Massage.jpeg" alt="email">
+                    EMAIL: hospiresc2022@gmail.com</p> 
+            </a>
+            <a>
+                <p class="text-white">  <img src="assets/icon/Kontak.jpeg" alt="email">
+                    NO HP: 0651-97495</p> 
+            </a>
+            <a>
+                <p class="text-white">  <img src="assets/icon/Twitter.jpeg" alt="email">
+                    Twitter: @hospiresc2022</p> 
+            </a>
+        </div>
     </div>
- 
-    
 
-  
-
-    
 @endsection
