4d4f4970cde76c780e42a3939e08b3768a00b6d7
Abdul Hafidh <abdul_h20@mhs.unsyiah.ac.id>
2022-04-23 15:45:55 +0700

    Make a Functionality of button

--- resources/views/pages/beranda.blade.php
+++ resources/views/pages/beranda.blade.php
@@ -5,30 +5,33 @@
 
 
 @section('content')
-   <div class="container bg-danger">
-    <div class="row">
-        <div class="col-8">
-            <img src="assets/images/kumpulan-doctor-removebg-preview.png" class="float-start" alt="tes">
-        </div>
-        <div class="col-4 text-white">
-            <h1>SELAMAT DATANG!</h1>
-                <div class="text-center">
-                    <h4>Lakukan Pengajuan Kebutuhan <br> Yang Anda Perlukan!</h4>
-                </div>
-                <br>
-                <div class="text-center">
-                    <p class="groove">
-                        Working Hours
-                        <br>
-                        Senin-Jum’at: 09.00-17.00<br>
-                        Sabtu-Minggu: 10.30-15.00
-                    </p>
-                    <a class="btn btn-primary" href="#" role="button">Link</a>
-                    
-        </div>
-      </div>
-    </div>
-   
+   <div class="container">
+       <div class="container mt-5">
+           <div class="row">
+               <div class="col-md-8">
+                   <img src="assets/images/kumpulan-doctor-removebg-preview.png" class="float-start" alt="tes">
+               </div>
+               <div class="col-4 text-white">
+                   <h1>SELAMAT DATANG!</h1>
+                       <div class="text-center">
+                           <h4>Lakukan Pengajuan Kebutuhan <br> Yang Anda Perlukan!</h4>
+                       </div>
+                       <br>
+                       <div class="text-center">
+                           <p class="groove">
+                               Working Hours
+                               <br>
+                               Senin-Jum’at: 09.00-17.00<br>
+                               Sabtu-Minggu: 10.30-15.00
+                           </p>
+                           
+                           <a href="/minta tolong"> <button type="button" class="btn btn-lg" style="background: #A1C4FD;">Ajukan</button></a>
+                           
+               </div>
+             </div>
+           </div>
+          
+       </div>
     
 </div>
 @endsection
