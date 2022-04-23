c313eef5fedc31796c8b37c9a221918852f83366
ulfarhmh17 <ulfa.r20@mhs.unsyiah.ac.id>
2022-04-23 13:01:40 +0700

    icon

--- resources/views/kontak.blade.php
+++ resources/views/kontak.blade.php
@@ -14,11 +14,11 @@
            Peunayong, Kec. Kuta Alam, Kota Banda Aceh, Aceh 
             </p>
         <div class="p-2  text-white">
-        <img src="icon/Massage.jpeg" height="35px" width="30px" href="assets/icon/Massage.jpeg" class="fs-4 text-light" >&nbsp;Hospirec2022@gmail.com</img></div>
+        <img src="icon/Massage.jpeg" height="35px" width="30px" href="public\assets\icon\Massage.jpeg" class="fs-4 text-light" >&nbsp;Hospirec2022@gmail.com</img></div>
         <div class="p-2 text-white">
-        <img src="icon/Kontak.jpeg" height="35px" width="30px" href="assets/icon/Kontak.jpeg" class="fs-4" >&nbsp;0651-9795</img></div>
+        <img src="icon/Kontak.jpeg" height="35px" width="30px" href="public\assets\icon\Kontak.jpeg" class="fs-4" >&nbsp;0651-9795</img></div>
         <div class="p-2 text-white">
-        <img src="icon/Twitter.jpeg" height="35px" width="30px" href="assets/icon/Twitter.jpeg" class="fs-4">&nbsp;@Hospirec2022</img></div>
+        <img src="icon/Twitter.jpeg" height="35px" width="30px" href="public\assets\icon\Twitter.jpeg" class="fs-4">&nbsp;@Hospirec2022</img></div>
         <div></div>
       </ul>
     </div>
