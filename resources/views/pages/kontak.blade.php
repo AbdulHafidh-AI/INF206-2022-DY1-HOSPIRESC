dc41c3496f44ee53720b2588382d32662a9ff456
ulfarhmh17 <ulfa.r20@mhs.unsyiah.ac.id>
2022-04-23 12:42:33 +0700

    modify kontak

--- resources/views/pages/kontak.blade.php
+++ resources/views/pages/kontak.blade.php
@@ -1,86 +1,31 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.home')
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+@include('partials.navbar2')
 
-    <title>HOSPIRESC</title>
-  </head>
-  <body>
-    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom bg-dark">
-        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-light text-decoration-none">
-          <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
-          <img src="icon/Logo.png" height="50px"  class="fs-4" >HOSPIRESC</img>
-        </a>
-        <ul class="nav nav-pills">
-            <li class="nav-item"><a href="#" class="nav-link text-light center"  aria-current="page">HOME</a></li>
-            <li class="nav-item"><a href="#" class="nav-link text-light">TENTANG KAMI</a></li>
-            <li class="nav-item"><a href="#" class="nav-link text-green">KONTAK</a></li>
-            <li class="nav-item"><a href="#" class="nav-link text-light">LOG IN</a></li>
-            <li class="nav-item"><a href="#" class="nav-link text-light"></a></li>
-          </ul>
-      </header>
+
+@section('content')
+
+
       <h1>Kontak</h1>
       <ul>
-        <p>&nbsp;&nbsp;Hubungan Kami</p>
+        <p>&nbsp;&nbsp;Hubungi Kami</p>
         <div class="p-2">
         <p>Jl. Jend. Ahmad Yani No.40
-            Peunayong, Kec. Kuta Alam, Kota Banda Aceh, Aceh 
+           Peunayong, Kec. Kuta Alam, Kota Banda Aceh, Aceh 
             </p>
         <div class="p-2  text-white">
-        <img src="icon/Massage.jpeg" height="35px" width="30px" href="" class="fs-4 text-light" >&nbsp;Hospirec2022@gmail.com</img></div>
+        <img src="icon/Massage.jpeg" height="35px" width="30px" href="assets/icon/Massage.jpeg" class="fs-4 text-light" >&nbsp;Hospirec2022@gmail.com</img></div>
         <div class="p-2 text-white">
-        <img src="icon/Kontak.jpeg" height="35px" width="30px" href="" class="fs-4" >&nbsp;0651-9795</img></div>
+        <img src="icon/Kontak.jpeg" height="35px" width="30px" href="assets/icon/Kontak.jpeg" class="fs-4" >&nbsp;0651-9795</img></div>
         <div class="p-2 text-white">
-        <img src="icon/Twitter.jpeg" height="35px" width="30px" href="" class="fs-4">&nbsp;@Hospirec2022</img></div>
+        <img src="icon/Twitter.jpeg" height="35px" width="30px" href="assets/icon/Twitter.jpeg" class="fs-4">&nbsp;@Hospirec2022</img></div>
         <div></div>
       </ul>
     </div>
  
     
 
-    <!-- Optional JavaScript; choose one of the two! -->
+  
 
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-    -->
-  <style>
-       body
-    {
-        background-image: url('images/image.png');
-        background-repeat: no-repeat;
-        background-size: cover;
-        
-        
-    }
-    h1
     
-    {
-        text-indent: 40px;
-        color: white;
-  font-family: "Times New Roman", Times, serif;
-}
-
-img {
-        color: white;
-  font-family: "Times New Roman", Times, serif;
-    }
-
-     
-
-    p {
-        color: white;
-  font-family: "Times New Roman", Times, serif;
-    }
-  </style>
-  </body>
-</html>
+@endsection
