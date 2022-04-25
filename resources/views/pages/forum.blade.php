b96e35959fce7f27bb511314b087506966712647
Abdul Hafidh <abdul_h20@mhs.unsyiah.ac.id>
2022-04-25 12:22:29 +0700

    Mengubah destinasi route

---
+++ resources/views/pages/forum.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.pertolongan')
+
+@include('partials.navbar2')
+
+@section('content')
+
+<div class="ms-5" style="width: 20%;">
+    <h1 class="text-bold text-white">KONTAK</h1>
+</div>
+
+
+@endsection
+
