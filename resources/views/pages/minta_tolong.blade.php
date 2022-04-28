bc149c8724bb41980b6ada867d13f646408a1b3d
Cut Nadilla Maretna <cut_nm@mhs.unsyiah.ac.id>
2022-04-28 18:18:24 +0700

    modify form

--- resources/views/pages/minta_tolong.blade.php
+++ resources/views/pages/minta_tolong.blade.php
@@ -4,8 +4,29 @@
 
 @section('content')
 
-<div class="ms-5" style="width: 70%;">
-    <h1 class="text-bold text-light title">MINTA TOLONG</h1>
+<div class="ms-5" style="width: 50%;">
+    <h2 class="text-bold text-light title">MINTA TOLONG</h2>
 </div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="text-center">
+<!-- formulir -->
+<form action="/login" method="POST">
+  <div class="text-start">
+  @csrf
+
+  
+    <label for="JudulPertolongan" class="form-label">JudulPertolongan</label><br>
+    <input type="text" class="form-control" id="JudulPertolongan" aria-describedby=" ">
+    <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
+  </div>
+  
+  <div class="mb-3">
+    <label for="DeskripsiPertolongan" class="form-label">DeskripsiPertolongan</label><br>
+    <input type="text" class="form-control" id="DeskripsiPertolongan">
+  </div>
+</form>
 
 @endsection
