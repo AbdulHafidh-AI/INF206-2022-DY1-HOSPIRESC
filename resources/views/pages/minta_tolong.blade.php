0998e903d8824a167aebc5602d58d1dde962ae41
Cut Nadilla Maretna <cut_nm@mhs.unsyiah.ac.id>
2022-04-28 18:24:18 +0700

    modify form

--- resources/views/pages/minta_tolong.blade.php
+++ resources/views/pages/minta_tolong.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.beranda')
+@extends('layouts.kontak')
 
 @include('partials.navbar2')
 
@@ -11,22 +11,42 @@
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-            <div class="text-center">
+            <div class="text-left">
 <!-- formulir -->
 <form action="/login" method="POST">
   <div class="text-start">
   @csrf
-
-  
-    <label for="JudulPertolongan" class="form-label">JudulPertolongan</label><br>
-    <input type="text" class="form-control" id="JudulPertolongan" aria-describedby=" ">
+    <label for="JudulPertolongan" class="form-label">Judul Pertolongan</label><br>
+    <input type="text" class="form-control" id="JudulPertolongan" >
     <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
   </div>
   
-  <div class="mb-3">
-    <label for="DeskripsiPertolongan" class="form-label">DeskripsiPertolongan</label><br>
+  <div class="mb-4">
+    <label for="DeskripsiPertolongan" class="form-label">Deskripsi Pertolongan</label><br>
     <input type="text" class="form-control" id="DeskripsiPertolongan">
   </div>
+
+  <div class="mb-4">
+    <label for="PilihAlatMedis" class="form-label">Pilih Alat Medis Yang Dibutuhkan</label>
+    <input type="text" class="form-control" id="PilihAlatMedis" ><select id="PilihAlatMedis">
+          <option>option1</option>
+          <option>option2</option>
+          <option>option3</option>
+          <option>option4</option>
+          <option>option5</option>
+          <option>option6</option>
+          <option>option7</option>
+          <option>option8</option>
+          <option>option1</option>
+          <option>option2</option>
+          <option>option3</option>
+          <option>option4</option>
+          <option>option5</option>
+          <option>option6</option>
+          <option>option7</option>
+          <option>option8</option>
+      </select>
+      </div>
 </form>
 
 @endsection
