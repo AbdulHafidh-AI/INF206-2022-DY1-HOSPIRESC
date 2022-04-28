a4bdb74f00136219eda37a3d407b1ccee499dfe7
rahminajla <rahmi_nj@mhs.unsyiah.ac.id>
2022-04-28 10:14:35 +0700

    filling riwayat file in layouts

---
+++ resources/views/layouts/riwayat.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css">
+
+
+    <!-- Custom CSS -->
+   <link rel="stylesheet" href="/assets/custom/login/riwayat.css">
+   <link rel="stylesheet" href="/assets/custom/navbar.css">
+
+    <!-- favicon -->
+    <link rel="shortcut icon" href="/assets/icon/Logo.png" type="image/x-icon">
+    
+
+
+    <title>{{ $title }} | HOSPIRESC</title>
+</head>
+<body>
+    <hr style="color: white">
+    @yield('content')
+    
+    <!-- Optional JavaScript -->
+    <script src="/assets/bootstrap/js/bootstrap.js"></script>
+    <script src="/assets//bootstrap/js/popper.min.js"></script>
+    
+</body>
+</html>
