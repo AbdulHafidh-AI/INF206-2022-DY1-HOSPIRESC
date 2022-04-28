b61bcdb1a49fcb3c8ca1001a25c02b43e99e1fec
Cut Nadilla Maretna <cut_nm@mhs.unsyiah.ac.id>
2022-04-28 19:38:21 +0700

    modify form

--- resources/views/pages/minta_tolong.blade.php
+++ resources/views/pages/minta_tolong.blade.php
@@ -4,6 +4,47 @@
 
 @section('content')
 
+<style>
+  .container {
+    font-family: 'PT Serif';
+    font-style: normal;
+    font-weight: 900;
+    font-size: 48px;
+    line-height: 64px;
+    display: flex;
+    align-items: center;
+    text-align: center;
+
+    color: #FFFFFF;
+}
+
+label {
+
+font-family: 'PT Serif';
+font-style: normal;
+font-weight: 700;
+font-size: 36px;
+line-height: 48px;
+
+}
+
+input {
+
+width: 676px;
+height: 74px;
+left: 338px;
+top: 523px;
+font-family: 'PT Serif';
+font-style: normal;
+font-weight: 700;
+font-size: 32px;
+line-height: 42px;
+
+background: #A1C4FD;
+border-radius: 15px;
+}
+</style>
+
 <div class="ms-5" style="width: 50%;">
     <h2 class="text-bold text-light title">MINTA TOLONG</h2>
 </div>
@@ -20,15 +61,11 @@
     <input type="text" class="form-control" id="JudulPertolongan" >
     <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
   
-  
-  
     <label for="DeskripsiPertolongan" class="form-label">Deskripsi Pertolongan</label><br>
     <input type="text" class="form-control" id="DeskripsiPertolongan">
   
-
-  
-    <label for="PilihAlatMedis" class="form-label">Pilih Alat Medis Yang Dibutuhkan</label>
-    <input type="text" class="form-control" id="PilihAlatMedis" ><select id="PilihAlatMedis">
+    <label for="PilihAlatMedis" class="form-label">Pilih Alat Medis Yang Dibutuhkan</label><br>
+    <select id="PilihAlatMedis">
           <option>option1</option>
           <option>option2</option>
           <option>option3</option>
