bd842f3dd72e049f21ed96faa0e8e3ed6169e5a0
Abdul Hafidh <abdul_h20@mhs.unsyiah.ac.id>
2022-05-01 14:29:11 +0700

    90 persen dah siap

--- routes/web.php
+++ routes/web.php
@@ -54,6 +54,8 @@ Route::get('/pages/riwayat', [PageController::class, 'riwayat'])->name('riwayat'
 Route::get('/pages/riwayat/{id}', [PertolonganController::class, 'edit'])->name('riwayat');
 // Jika user menekan tombol info pada daftar card maka akan diarahkan ke halaman info jika sesi login true
 Route::get('/pages/info/{id}', [PertolonganController::class, 'show'])->name('info');
+// Jika user menekan tombol detail pada daftar card maka akan diarahkan ke halaman detail jika sesi login true
+Route::get('/pages/detail/{id}', [PertolonganController::class, 'detail'])->name('detail');
 
 
 
