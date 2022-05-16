96ed99e065425c8406480288a79bdced5f001cd3
Abdul Hafidh <abdul_h20@mhs.unsyiah.ac.id>
2022-05-16 07:36:21 +0700

    revisi semua tampilan halaman yang kurang bagus

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,38 +1,3 @@
 @extends('layouts.home')
 
 @include('partials.navbar')
-
-@section('content')
-    
-<div class="container mt-5">
-    
-        <div class="container">
-            <div class="kolom">
-                <div class="text-center"></div>
-                <p class="deskripsi">
-                    <h1>Minta Tolong Jadi<br>
-                    Lebih Mudah dengan <br>
-                    HOSPIRESC</h1>
-                </p>
-
-                <div class="text-center"></div>
-                <p class="des1">
-                    <h3>HOSPIRESC adalah layanan aplikasi berbasis web yang <br>
-                    diperuntukkan bagi  setiap rumah sakit di seluruh <br> 
-                    Indonesia.
-                    </h3>
-                </p>
-
-                <p class="des2style=">
-                    <h3> HOSPIRESC sebagai penghubung antar rumah sakit di <br>    
-                    dalam  suatu forum aplikasi agar dapat saling <br> 
-                    melakukan interaksi  mengenai kebutuhan peralatan <br> 
-                    sebagai penunjang peralatan  dan pelayanan pasien.</h3>
-                </p>
-
-                
-            </div>
-         </div>
-         <img src="/assets/images/doctor.png" alt=" " class="rounded float-end" style="width:50%">
-</div>
-@endsection
--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -1,35 +1,49 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <!-- Required meta tags -->
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css">
-
-
-    <!-- Custom CSS -->
-    <link rel="stylesheet" href="/assets/custom/template/style.css">
-    <link rel="stylesheet" href="/assets/custom/login/style.css">
-    <link rel="stylesheet" href="/assets/custom/home/style.css">
-    <link rel="stylesheet" href="/assets/custom/beranda/style.css">
-
-   <!-- favicon -->
-   <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
-    
-
-
-    <title>{{ $title }} | HOSPIRESC</title>
-</head>
-<body>
-    <hr style="color: white">
-    @yield('content')
-    
-    <!-- Optional JavaScript -->
-    <script src="/assets/bootstrap/js/bootstrap.js"></script>
-    <script src="/assets//bootstrap/js/popper.min.js"></script>
-    
-</body>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Scrolling Nav - Start Bootstrap Template</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="/home/css/styles.css" rel="stylesheet" />
+        <link rel="stylesheet" href="/assets/custom/navbar.css">
+    </head>
+    <body class="d-flex flex-column min-vh-100">
+        <!-- Header-->
+        <header class=" bg-gradient text-white" style="background-color: #2F303A;">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Selamat Datang di Aplikasi HOSPIRESC</h1>
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="about">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-6">
+                        <p class="lead bold">HOSPIRESC adalah layanan aplikasi berbasis web yang
+                            diperuntukkan bagi  setiap rumah sakit di seluruh 
+                            Indonesia.</p>
+                        <p class="lead">
+                            HOSPIRESC adalah sebuah aplikasi yang bertujuan agar sesama rumah sakit dapat saling menolong untuk berbagi dan membagi alat medis sederhana. Dengan adanya aplikasi ini rumah sakit dapat dengan mudah untuk meminta pertolonganan alat-alat medis sederhana saat dibutuhkan.
+                        </p>
+                    </div>
+                    <div class="col-lg-6">
+                        <img src="/assets/images/doctor.png" alt=" " class="rounded float-end" style="width:50%">
+                    </div>
+                </div>
+            </div>
+            
+        </section>
+        <!-- Footer-->
+        <footer class="py-5 bg-dark mt-auto">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; HOSPIRESC 2022</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="/home/js/scripts.js"></script>
+    </body>
 </html>
